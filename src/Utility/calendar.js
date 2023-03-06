6af4f42303e1ecb88d0eaeaf4034ac72a0f1d00f
ONLYiiii <nitwaree111144@gmail.com>
2023-03-06 16:24:19 +0700

    test Push&Pull

--- src/Utility/calendar.js
+++ src/Utility/calendar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 function calendar() {
-    
+    return (
+        <View>
+            <Text>Test Push&Pull</Text>
+        </View>
+    )
 }
 
 export default calendar;
