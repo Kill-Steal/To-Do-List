cb9e09ffb065816e0ec0aaba091ba182cb5f94c9
Kill-Steal <peezacub@hotmail.com>
2023-03-06 17:43:20 +0700

    tested push&pull

--- src/Utility/calendar.js
+++ src/Utility/calendar.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 function calendar() {
     return (
         <View>
-            <Text>Test Push&Pull</Text>
+            
         </View>
     )
 }
